dd5636c03b6be42e80c9239862f28914dff42542
tungbinhdinh89 <tungbinhdinh89@gmail.com>
2024-05-10 09:13:18 +0700

    fix issue

--- server/api/controllers/auth.controller.js
+++ server/api/controllers/auth.controller.js
@@ -66,7 +66,7 @@ export const google = async (req, res, next) => {
   const { name, email, googlePhotoUrl } = req.body;
 
   try {
-    const user = await User.findOne(email);
+    const user = await User.findOne({ email });
 
     if (user) {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
