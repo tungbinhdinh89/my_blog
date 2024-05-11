783966400691527c82c64febf011f1406df01f5e
tungbinhdinh89 <tungbinhdinh89@gmail.com>
2024-05-11 09:51:28 +0700

    add profile dashboard ui

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -6,15 +6,15 @@ import DashProfile from "../componets/DashProfile";
 export default function Dashboard() {
   const location = useLocation();
   const [tab, setTab] = useState("");
+
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
     const tabFromUrl = urlParams.get("tab");
-
     if (tabFromUrl) {
       setTab(tabFromUrl);
     }
-    console.log(tabFromUrl);
   }, [location.search]);
+
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
       <div className="md:w-56">
