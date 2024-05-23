7d4f91b5fa9a012510b351482bdef77833ba8497
tungbinhdinh89 <tungbinhdinh89@gmail.com>
2024-05-23 16:17:49 +0700

    hidden search icon lg

--- client/src/componets/Header.jsx
+++ client/src/componets/Header.jsx
@@ -45,7 +45,7 @@ export default function Header() {
           className="hidden lg:inline"
         />
       </form>
-      <Button className="w-12 h-10" color="gray" pill>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
         <AiOutlineSearch />
       </Button>
       <div className="flex gap-2 md:order-2">
