9d029a80d1aa791c7f56a1d32b79990daf8b71d5
tungbinhdinh89 <tungbinhdinh89@gmail.com>
2024-05-29 20:14:53 +0700

    add comment section to the post page

---
+++ client/src/componets/CommentSection.jsx
@@ -0,0 +1,89 @@
+import { Alert, Button, Textarea } from "flowbite-react";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+export default function CommentSection({ postId }) {
+  const { currentUser } = useSelector((state) => state.user);
+  const [comment, setComment] = useState("");
+  const [commentError, setCommentError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (comment.length > 200) return;
+    try {
+      const res = await fetch(`/api/comment/create`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          userId: currentUser._id,
+          postId,
+          content: comment,
+        }),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (res.ok) {
+        setComment("");
+        setCommentError(null);
+      }
+    } catch (error) {
+      setCommentError(error.message);
+    }
+  };
+
+  return (
+    <div className="max-w-2xl mx-auto w-full p-3">
+      {currentUser ? (
+        <div className="flex items-center gap-1 my-5 text-gray-500 text-sm">
+          <p>Signed in as:</p>
+          <img
+            className="h-5 w-5 object-cover rounded-full"
+            src={currentUser.profilePicture}
+            alt={currentUser.username}
+          />
+          <Link
+            to={`/dashboard?tab=profile`}
+            className="text-xs text-cyan-600 hover:underline"
+          >
+            @{currentUser.username}
+          </Link>
+        </div>
+      ) : (
+        <div className="text-sm text-teal-500 my-5 flex gap-1 ">
+          You must be signed in to comment.
+          <Link className="text-blue-500 hover:underline" to={`/sign-in`}>
+            Sign In
+          </Link>
+        </div>
+      )}
+      {currentUser && (
+        <form
+          className="border border-teal-500 rounded-md p-3"
+          onSubmit={handleSubmit}
+        >
+          <Textarea
+            placeholder="Add a comment..."
+            rows="3"
+            maxLength="300"
+            onChange={(e) => setComment(e.target.value)}
+            value={comment}
+          />
+          <div className="flex items-center justify-between mt-5">
+            <p className="text-gray-500 text-xs">
+              {300 - comment.length} characters reamaining
+            </p>
+            <Button outline gradientDuoTone="purpleToBlue" type="submit">
+              Submit
+            </Button>
+          </div>
+          {commentError && (
+            <Alert color="failure" className="mt-5">
+              {commentError}
+            </Alert>
+          )}
+        </form>
+      )}
+    </div>
+  );
+}
